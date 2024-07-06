53190081323ca7f916d670d8b0b722b65b302c21
santiagolopez <dev.santiagolopez@gmail.com>
2024-07-06 15:36:44 -0500

    Second Commit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,50 @@ import pygame
 import sys
 from player import Player
 
+def load_spritesheet(filename, width, height):
+    sheet = pygame.image.load(filename).convert_alpha()
+    sheet_width, sheet_height = sheet.get_size()
+    cols = sheet_width // width
+    rows = sheet_height // height
+
+    sprites = []
+    for y in range(rows):
+        row_sprites = []
+        for x in range(cols):
+            rect = pygame.Rect(x * width, y * height, width, height)
+            sprite = sheet.subsurface(rect)
+            row_sprites.append(sprite)
+        sprites.append(row_sprites)
+    
+    return sprites
+
+def load_all_sprites():
+    walk_spritesheet = load_spritesheet('../assets/images/Unarmed_Walk/Unarmed_Walk_full.png', 64, 64)
+    run_spritesheet = load_spritesheet('../assets/images/Unarmed_Run/Unarmed_Run_full.png', 64, 64)
+    idle_spritesheet = load_spritesheet('../assets/images/Unarmed_Idle/Unarmed_Idle_full.png', 64, 64)
+    
+    sprite_data = {
+        'walk': {
+            'down': walk_spritesheet[0],
+            'left': walk_spritesheet[1],
+            'right': walk_spritesheet[2],
+            'up': walk_spritesheet[3]
+        },
+        'run': {
+            'down': run_spritesheet[0],
+            'left': run_spritesheet[1],
+            'right': run_spritesheet[2],
+            'up': run_spritesheet[3]
+        },
+        'idle': {
+            'down': idle_spritesheet[0],
+            'left': idle_spritesheet[1],
+            'right': idle_spritesheet[2],
+            'up': idle_spritesheet[3]
+        }
+    }
+    return sprite_data
+
 # Inicializa Pygame
 pygame.init()
 
@@ -21,8 +65,11 @@ NEGRO = (0, 0, 0)
 background = pygame.image.load('../assets/images/map.png')
 background = pygame.transform.scale(background, (screen_width, screen_height))  # Ajusta el tamaño del mapa si es necesario
 
+# Cargar los sprites desde los spritesheets
+sprites = load_all_sprites()
+
 # Crear el jugador
-player = Player(100, 100)
+player = Player(100, 100, sprites)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
--- src/player.py
+++ src/player.py
@@ -1,20 +1,71 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, sprites):
         super().__init__()
-        self.image = pygame.image.load('../assets/images/player.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (32, 32))  # Ajusta a 32x32 píxeles
+        self.sprites = sprites
+        self.action = 'idle'
+        self.direction = 'down'
+        self.frame = 0
+        self.image = self.get_sprite()
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        self.speed = 1
+        self.speed = 0  # Reducir la velocidad de movimiento del personaje
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 200  # Aumentar este valor para hacer la animación más lenta
+        self.is_moving = False
+        self.idle_timer = pygame.time.get_ticks()
+        self.idle_animation_interval = 30000  # 30 segundos en milisegundos
 
     def update(self, keys):
+        self.handle_movement(keys)
+        self.animate()
+
+    def handle_movement(self, keys):
+        self.is_moving = False  # Resetear el estado de movimiento
+        self.action = 'idle'
+
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
+            self.direction = 'left'
+            self.action = 'walk'
+            self.is_moving = True
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
+            self.direction = 'right'
+            self.action = 'walk'
+            self.is_moving = True
         if keys[pygame.K_UP]:
             self.rect.y -= self.speed
+            self.direction = 'up'
+            self.action = 'walk'
+            self.is_moving = True
         if keys[pygame.K_DOWN]:
             self.rect.y += self.speed
+            self.direction = 'down'
+            self.action = 'walk'
+            self.is_moving = True
+
+        # Cambiar a correr si se presiona Shift
+        if self.is_moving and keys[pygame.K_LSHIFT]:
+            self.action = 'run'
+
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_rate:
+            self.last_update = now
+            if self.is_moving:
+                self.frame = (self.frame + 1) % len(self.sprites[self.action][self.direction])
+            else:
+                if now - self.idle_timer > self.idle_animation_interval:
+                    self.idle_timer = now
+                    self.frame = (self.frame + 1) % len(self.sprites['idle'][self.direction])
+                else:
+                    self.frame = 0
+
+            self.image = self.get_sprite()
+
+    def get_sprite(self):
+        action_sprites = self.sprites[self.action][self.direction]
+        sprite = action_sprites[self.frame]
+        return sprite
